31694f840dda7b938a62c1692c713c791cd0f779
mahnoorwarsi <mahnoorwarsi@gmail.com>
2018-10-06 21:19:30 -0400

    final touch required

---
+++ conversion.h
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+#include<string>
+#include"stack.h"
+using namespace std;
+class conversion
+{
+
+
+
+
+};
---
+++ node.h
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+class node
+{
+  private:
+  int data;
+  node * next;
+  public:
+};
